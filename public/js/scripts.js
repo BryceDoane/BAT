0dccbf3a8e11a9f2210968759fb10290dda806f9
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-09-10 08:49:48 -0400

    Added firebase javascript

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -60,4 +60,21 @@
     }
   });
 
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+  const firebaseConfig = {
+    apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
+    authDomain: "behavior-analysis-tracker.firebaseapp.com",
+    databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
+    projectId: "behavior-analysis-tracker",
+    storageBucket: "behavior-analysis-tracker.appspot.com",
+    messagingSenderId: "392015561610",
+    appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
+    measurementId: "G-EM4XVKW2YS"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
 })(jQuery); // End of use strict
