380db33a4d09bb2b727fa8c2f2b609b1bac0eaf5
GageSapp-Rahme <61480697+GageSapp-Rahme@users.noreply.github.com>
2020-09-11 10:36:33 -0400

    added functions
    an event in the submit button to trigger the js file and functions which in theory will push the user new login info to firebase

--- public/login.js
+++ public/login.js
@@ -9,3 +9,53 @@ const firebaseConfig = {
     appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
     measurementId: "G-EM4XVKW2YS"
   };
+
+  firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('users');
+
+// Listen for form submit
+document.getElementById('signup').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var fname = getInputVal('fname');
+  var lname = getInputVal('lname');
+  var email = getInputVal('email');
+  var password = getInputVal('password');
+  
+
+  // Save user
+  saveMessage(fname, lname, email, password);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('signup').reset();
+}
+
+// Function to get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save user to firebase
+function saveMessage(fname, lname, email, password){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    fname: fname,
+    lname:lname,
+    email:email,
+    password:password
+  });
+}
