0b9e84c78834d52bd52617bf1fec5de328879e45
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-09-10 23:13:47 -0400

    added login.js

---
+++ public/login.js
@@ -0,0 +1,11 @@
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+const firebaseConfig = {
+    apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
+    authDomain: "behavior-analysis-tracker.firebaseapp.com",
+    databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
+    projectId: "behavior-analysis-tracker",
+    storageBucket: "behavior-analysis-tracker.appspot.com",
+    messagingSenderId: "392015561610",
+    appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
+    measurementId: "G-EM4XVKW2YS"
+  };
