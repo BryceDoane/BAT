b372bfe7635759330b2b8181cc2112074f6b2ea7
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-09-15 12:20:54 -0400

    added login js

---
+++ public/js/login.js
@@ -0,0 +1,102 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
+    authDomain: "behavior-analysis-tracker.firebaseapp.com",
+    databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
+    projectId: "behavior-analysis-tracker",
+    storageBucket: "behavior-analysis-tracker.appspot.com",
+    messagingSenderId: "392015561610",
+    appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
+    measurementId: "G-EM4XVKW2YS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('users');
+var loginTest = firebase.database().ref('test')
+
+/*
+// Listen for form submit
+document.getElementById('signup').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+  // Get values
+  var fname = getInputVal('fname');
+  var email = getInputVal('email');
+  var password = getInputVal('password'); 
+  
+  saveMessage(fname,email,password);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('signup').reset();
+
+
+}
+
+// Function to get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(fname, email, password){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    fname: fname,
+    email:email,
+    password:password
+  });
+}
+*/
+  //Listens for the login action on login button
+   document.getElementById('login').addEventListener('login', submitForm);
+
+  function submitForm(e){
+    e.preventDefault();
+  
+      // Get values
+    var email = getInputVal('email');
+    var password = getInputVal('password');
+
+    
+    saveLogin(email,password);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('login').reset();
+  
+  }
+  
+  // Function to get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+/*
+  var databaseEmail = firebase.database().ref(email);
+  var databasePassword = firebase.database().ref(password);
+    function loginTest(email, password){
+      var loginTest = loginTest.push();
+      loginTest.set({
+        databaseEmail:email,
+        databasePassword:password
+  }) 
+ }*/
