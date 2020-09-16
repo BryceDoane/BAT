77995b34a8cb68b4386559282b93171a6d8c0b1e
eddiedaniel <61479529+eddiedaniel@users.noreply.github.com>
2020-09-16 12:05:12 -0400

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -16,40 +16,10 @@
 
   function signIn(){
   
-    const promise = auth.signInWithEmailAndPassword(email, password);
-    promise.catch(e => alert(e.message));
-    
-   }
-   
-   
-   function signOut(){
-    
-    auth.signOut();
-    alert("Signed Out");
-    
-   }
-   
-   
-   
-   auth.onAuthStateChanged(function(user){
-    
-    if(user){
-     
-     var email = user.email;
-     alert("Active User " + email);
-     
-     //Take user to a different or home page
-  
-     //is signed in
-     
-    }else{
-     
-     alert("No Active User");
-     //no user is signed in
-    }
-    
-    
-    
-   });
-  
-  
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
