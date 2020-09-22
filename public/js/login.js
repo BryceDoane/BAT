f2713b7e7fcfe901f344e37afc3853f1d822b421
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-09-22 12:50:32 -0400

    just formatted code

--- public/js/login.js
+++ public/js/login.js
@@ -1,67 +1,67 @@
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
-    authDomain: "behavior-analysis-tracker.firebaseapp.com",
-    databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
-    projectId: "behavior-analysis-tracker",
-    storageBucket: "behavior-analysis-tracker.appspot.com",
-    messagingSenderId: "392015561610",
-    appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
-    measurementId: "G-EM4XVKW2YS"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
+  authDomain: "behavior-analysis-tracker.firebaseapp.com",
+  databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
+  projectId: "behavior-analysis-tracker",
+  storageBucket: "behavior-analysis-tracker.appspot.com",
+  messagingSenderId: "392015561610",
+  appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
+  measurementId: "G-EM4XVKW2YS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 /**
      * Handles the sign in button press.
      */
-    function toggleSignIn() {
-      if (firebase.auth().currentUser) {
-      // [START signout]
-      firebase.auth().signOut();
-      // [END signout]
-      } else {
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
-      if (email.length < 4) {
+function toggleSignIn() {
+  if (firebase.auth().currentUser) {
+    // [START signout]
+    firebase.auth().signOut();
+    // [END signout]
+  } else {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
       alert('Please enter an email address.');
       return;
-      }
-      if (password.length < 4) {
+    }
+    if (password.length < 4) {
       alert('Please enter a password.');
       return;
-      }
-      // Sign in with email and pass.
-      // [START authwithemail]
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    }
+    // Sign in with email and pass.
+    // [START authwithemail]
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       // [START_EXCLUDE]
       if (errorCode === 'auth/wrong-password') {
-      alert('Error: Wrong password.');
+        alert('Error: Wrong password.');
       } else {
-      alert(errorMessage);
+        alert(errorMessage);
       }
       console.log(error);
       document.getElementById('loginbtn').disabled = false;
       // [END_EXCLUDE]
-      });
-      // [END authwithemail]
-      }
-      document.getElementById('loginbtn').disabled = true;
-      }
-      /**
-           * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-           *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-           *    out, and that is where we update the UI.
-           */
-      function initApp() {
-      // Listening for auth state changes.
-      // [START authstatelistener]
-      firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
+    });
+    // [END authwithemail]
+  }
+  document.getElementById('loginbtn').disabled = true;
+}
+/**
+     * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+     *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+     *    out, and that is where we update the UI.
+     */
+function initApp() {
+  // Listening for auth state changes.
+  // [START authstatelistener]
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
       // User is signed in.
       var email = user.email;
       alert("signed in as " + email);
@@ -70,7 +70,7 @@
       document.getElementById('loginbtn').textContent = 'Sign out';
 
       // [END_EXCLUDE]
-      } else {
+    } else {
       // User is signed out.
       // [START_EXCLUDE]
       document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
@@ -78,18 +78,17 @@
       document.getElementById('signed-in-user').textContent = "";
       console.log("signed out");
       // [END_EXCLUDE]
-      }
-      // [START_EXCLUDE silent]
-      document.getElementById('loginbtn').disabled = false;
-      // [END_EXCLUDE]
-      });
-      // [END authstatelistener]
-      document.getElementById('loginbtn').addEventListener('click', toggleSignIn, false);
-      }
-      window.onload = function() {
-      initApp();
-      };
-    
+    }
+    // [START_EXCLUDE silent]
+    document.getElementById('loginbtn').disabled = false;
+    // [END_EXCLUDE]
+  });
+  // [END authstatelistener]
+  document.getElementById('loginbtn').addEventListener('click', toggleSignIn, false);
+}
+window.onload = function () {
+  initApp();
+};
 
-   
-  
+
+
