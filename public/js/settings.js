9294b352884b1c523021f5d2060ac1dd6547976e
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-09-30 19:43:42 -0400

    verify email added

---
+++ public/js/settings.js
@@ -0,0 +1,27 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
+    authDomain: "behavior-analysis-tracker.firebaseapp.com",
+    databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
+    projectId: "behavior-analysis-tracker",
+    storageBucket: "behavior-analysis-tracker.appspot.com",
+    messagingSenderId: "392015561610",
+    appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
+    measurementId: "G-EM4XVKW2YS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+function verifyEmail(){
+    var user = firebase.auth().currentUser;
+    alert(user.email);
+
+user.sendEmailVerification().then(function() {
+  // Email sent.
+}).catch(function(error) {
+    alert(error);
+  // An error happened.
+});
+}
