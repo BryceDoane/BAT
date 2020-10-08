0a93ec82ea7094a2aaaec2a37123305b0d5afa7e
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-10-08 12:36:25 -0400

    add class adds UID of adder

--- public/js/class.js
+++ public/js/class.js
@@ -1,17 +1,17 @@
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
-    authDomain: "behavior-analysis-tracker.firebaseapp.com",
-    databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
-    projectId: "behavior-analysis-tracker",
-    storageBucket: "behavior-analysis-tracker.appspot.com",
-    messagingSenderId: "392015561610",
-    appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
-    measurementId: "G-EM4XVKW2YS"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyB9Y2gGnOUC9tG_4piaqqCEhMxdi5yxQDI",
+  authDomain: "behavior-analysis-tracker.firebaseapp.com",
+  databaseURL: "https://behavior-analysis-tracker.firebaseio.com",
+  projectId: "behavior-analysis-tracker",
+  storageBucket: "behavior-analysis-tracker.appspot.com",
+  messagingSenderId: "392015561610",
+  appId: "1:392015561610:web:d9d2686cb3c9b312e4fe73",
+  measurementId: "G-EM4XVKW2YS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Get the modal
 var classModal = document.getElementById("classModal");
@@ -23,28 +23,39 @@ var addClassbtn = document.getElementById("addClassModal");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-addClassbtn.onclick = function() {
+addClassbtn.onclick = function () {
   classModal.style.display = "block";
 }
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   classModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == classModal) {
     classModal.style.display = "none";
   }
 }
+var uid;
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    uid = user.uid;
+    console.log(uid);
+  } else {
+    // No user is signed in.
+  }
+});
 
 //Takes in class name from modal form
 function newClass() {
-    var className = document.getElementById("className").value;
-    alert(className);
-    classModal.style.display = "none";
-    firebase.database().ref('classes').push({className:className, task:"temp"});
-  }
-  
+  var className = document.getElementById("className").value;
+  console.log(uid);
+  //var UID = user.uid;
+  alert(className);
+  classModal.style.display = "none";
+  firebase.database().ref('classes').push({ className: className, task: "temp", UID: uid });
+}
