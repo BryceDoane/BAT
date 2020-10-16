1fca9afb97fbc409a042aed0669e7e3136906186
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-10-15 23:40:14 -0400

    fixed class and task modal

--- public/js/class.js
+++ public/js/class.js
@@ -15,41 +15,27 @@ firebase.initializeApp(firebaseConfig);
 
 // Get the modal
 var classModal = document.getElementById("classModal");
-var taskModal = document.getElementById("taskModal");
 
 // Get the button that opens the modal
 var addClassbtn = document.getElementById("addClassModal");
-var addTaskBtn = document.getElementById("addTaskModal");
 
 // Get the <span> element that closes the modal
 var span1 = document.getElementsByClassName("close")[0];
-var span2 = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 addClassbtn.onclick = function () {
   classModal.style.display = "block";
-}
-addTaskBtn.onclick = function () {
-  taskModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span1.onclick = function () {
   classModal.style.display = "none";
 }
-span2.onclick = function () {
-  taskModal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == classModal) {
     classModal.style.display = "none";
-  }
-}
-window.onclick = function (event) {
-  if (event.target == taskModal) {
-    taskModal.style.display = "none";
   }
 }
 
@@ -91,8 +77,8 @@ function newTask() {
   var select = document.getElementById("classList");
   var classSelect = select.options[select.selectedIndex].value;
   var classRef2 = firebase.database().ref('classes/' + classSelect + "/Tasks").child(taskName);
-  taskModal.style.display = "none";
-  classRef2.set("3");
+  classModal.style.display = "none";
+  classRef2.set("");
   location.reload();
 }
 //uid = "gkOIuUEI7lZSto7eEgwHMywlc1A2";
