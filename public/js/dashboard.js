7f5fa4b0d339005f864bcf847f0c055523c69ea4
GageSapp-Rahme <61480697+GageSapp-Rahme@users.noreply.github.com>
2020-11-03 17:03:34 -0500

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -49,21 +49,16 @@ firebase.auth().onAuthStateChanged(function (user) {
         classesList = classes.toString();
 
       });
-      
+      var studentRef = firebase.database().ref('Schools/' + "null" + "/classes" + "/Test Class 2" + '/Student List');
+      studentRef.on('value', function (snapshot){
+        var studentTest = snapshot.toString();
+        console.log(studentTest);
+      });
       for (i = 0; i < classes.length; i++) {
         var node = document.createElement('ul');
         node.classList.add("classList"); /*adds classList as class on ul*/
         var textNode = document.createTextNode(classes[i]);
-        var studentRef = firebase.database().ref('Schools/' + schoolName + "/classes" + "/" + classes[i] + '/Student List');
-        // studentRef.on('value', function (snapshot) {
-        //   snapshot.forEach(function (childSnapshot) {
-        //   var childSData = childSnapshot.val();
-        //   students.push(childCSData.studentcid);
-        //   studentList = students.toString();
-  
-        // });
         node.appendChild(textNode);
-        //node.appendChild(studentList);
         document.getElementById("classNameLi").appendChild(node);
       }
       //document.getElementById("classNameLi").innerHTML = classesList;
@@ -162,7 +157,7 @@ firebase.auth().onAuthStateChanged(function (user) {
   //uid = user.uid;
  // email = user.email;
   schoolName = user.displayName;
-  var tasksRef = firebase.database().ref("Schools/" + schoolName + "/classes/" + textNode + "/tasks/" );
+  var tasksRef = firebase.database().ref("Schools/" + schoolName + "/classes/" + classesList + "/");
   tasksRef.on('value', function (snapshot) {
     console.log(snapshot);
     snapshot.forEach(function (childSnapshot) {
