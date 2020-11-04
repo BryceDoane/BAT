ff1b826a18358beae53ff927ccaa275f9501130d
Bryce Doane <brycedoane@Bryces-MacBook-Pro.local>
2020-11-04 11:44:08 -0500

    dashboard shows tasks

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -48,167 +48,185 @@ firebase.auth().onAuthStateChanged(function (user) {
     var classRef = firebase.database().ref('Schools/' + schoolName + "/classes");
     var studentNameRef = firebase.database().ref('Schools/' + schoolName + "/students");
     studentNameRef.on('value', function (snapshot) {
-    snapshot.forEach(function (childSnapshot) {
-    var childSNData = childSnapshot.val();
-    studentNames.push(childSNData.studentName);
-    studentNames.push(childSNData.studentID);
-  });
-  studentsNamesList = studentNames; //.toString();
-  console.log(studentsNamesList);
-  });
-      classRef.on('value', function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-          var childCData = childSnapshot.val();
-          var select = document.getElementById("classList")
-          classRef.on('child_added', function (snapshot) {
-            //Do something with the data
-            //document.getElementById("classNameLi").innerHTML = childData.className;
-
-          });
-          classes.push(childCData.className);
-          classesList = classes.toString();
+      snapshot.forEach(function (childSnapshot) {
+        var childSNData = childSnapshot.val();
+        studentNames.push(childSNData.studentName);
+        studentNames.push(childSNData.studentID);
+      });
+      studentsNamesList = studentNames; //.toString();
+      //console.log(studentsNamesList);
+    });
+    classRef.on('value', function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        var childCData = childSnapshot.val();
+        var select = document.getElementById("classList")
+        classRef.on('child_added', function (snapshot) {
+          //Do something with the data
+          //document.getElementById("classNameLi").innerHTML = childData.className;
 
         });
-        
-        for (i = 0; i < classes.length; i++) {
+        classes.push(childCData.className);
+        classesList = classes.toString();
+
+      });
+
+      for (i = 0; i < classes.length; i++) {
         students = [];
-        
+
         var studentRef = firebase.database().ref('Schools/' + schoolName + "/classes" + "/" + classes[i] + "/Student List");
         studentRef.on('value', function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-          var childSData = childSnapshot.val();
-          var identifier = childSData.studentcid;
-          var finder = studentsNamesList.indexOf(identifier);
-          finder = finder -1;
-          students.push(studentsNamesList[finder]);
-          
+          snapshot.forEach(function (childSnapshot) {
+            var childSData = childSnapshot.val();
+            var identifier = childSData.studentcid;
+            var finder = studentsNamesList.indexOf(identifier);
+            finder = finder - 1;
+            students.push(studentsNamesList[finder]);
+
+          });
+          studentsList = students.toString();
+          //console.log(studentsList);
         });
-        studentsList = students.toString();
-        console.log(studentsList);
-        });
-          var node = document.createElement('ul');
-          node.classList.add("classList"); /*adds classList as class on ul*/
-          var textNode = document.createTextNode("Class: " + classes[i] + " Students: " + studentsList);
-          node.appendChild(textNode);
-          document.getElementById("classNameLi").appendChild(node);
+        var node = document.createElement('ul');
+        node.classList.add("classList"); /*adds classList as class on ul*/
+        var textNode = document.createTextNode("Class: " + classes[i] + " Students: " + studentsList);
+        node.appendChild(textNode);
+        document.getElementById("classNameLi").appendChild(node);
 
-        
-        }
-        //document.getElementById("classNameLi").innerHTML = classesList;
-      });
-    
-};
-});
-//Create table
-var mountains = [];
 
-function generateClassesTable(mountains) {
-
-  let table = document.querySelector("table");
-  let data = Object.keys(mountains[0]);
-  generateTable(table, mountains);
-  generateTableHead(table, data);
-
-  function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
-    }
-  }
-
-  function generateTable(table, data) {
-    for (let element of data) {
-      let row = table.insertRow();
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
       }
-    }
-  }
-  mountains = [];
-}
+      for (i = 0; i <= classes.length; i++) {
+        var ctext = classes[i];//document.getElementById('classNameLi').nodevalue;
+        var tasksRef = firebase.database().ref("Schools/" + schoolName + "/classes/" + ctext + "/Tasks");
 
-var classesRef = firebase.database().ref('classes');
-var classesList;
- classesRef.on('value', function (snapshot) {
-  snapshot.forEach(function (childSnapshot) {
-    var childData = childSnapshot.val();
-    classesRef.on('child_added', function (snapshot) {
-      //Do something with the data
-      //document.getElementById("classNameLi").innerHTML = childData.className;
+        tasksRef.on('value', function (snapshot) {
+          //console.log(snapshot.val());
+          snapshot.forEach(function (childSnapshot) {
+            tasks.push(ctext + ":");
+            tasks.push(childSnapshot.val().taskName);
+            //console.log(childData);
+            taskList = tasks.toString();
 
+          });
+          document.getElementById("taskNameLi").innerHTML = taskList;
+        });
+        //document.getElementById("classNameLi").innerHTML = classesList;
+      }
     });
-   classes.push(childData.className);
-    classes.push(childData.Tasks);
-  });
-  for (i = 0; i < (classes.length / 2); i++) {
-    var td = document.createElement('TABLE');
-    document.getElementById("classNameLi").appendChild(td);
-    mountains = [
-      { StudentName: "Bryce", Bring_Pencils: 1, Bring_Homework: 1, Task4: 3, task3: 9 },
-      { name: "Gage", task1: 4, task2: 5, task3: 3, task4: 6 },
-      { name: "Matt", task1: 2, task2: 2, task3: 3, task4: 6 },
-      { name: "Eddie", task1: 5, task2: 3, task3: 3, task4: 6 },
-      { name: "Pat", task1: 3, task2: 4, task3: 3, task4: 6 }
-    ];
 
-  }
+  };
 
-  generateClassesTable(mountains);
-});
+  //Create table
+  // var mountains = [];
 
-//log out functionality on top right
-function signout() {
-  firebase.auth().signOut();
-  alert("signed out");
-};
-const sout = document.getElementById("lout");
-sout.addEventListener('click', signout);
+  // function generateClassesTable(mountains) {
 
-//gets signed in user
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user != null) {
-    document.getElementById("linu").innerHTML = user.email;
-  } else {
-    document.getElementById("linu");
-  }
-})
+  //   let table = document.querySelector("table");
+  //   let data = Object.keys(mountains[0]);
+  //   generateTable(table, mountains);
+  //   generateTableHead(table, data);
 
+  //   function generateTableHead(table, data) {
+  //     let thead = table.createTHead();
+  //     let row = thead.insertRow();
+  //     for (let key of data) {
+  //       let th = document.createElement("th");
+  //       let text = document.createTextNode(key);
+  //       th.appendChild(text);
+  //       row.appendChild(th);
+  //     }
+  //   }
 
-//Show tasks as table
-/*firebase.auth().onAuthStateChanged(function (user) {
-  //if (user) {
-  //uid = user.uid;
- // email = user.email;
+  //   function generateTable(table, data) {
+  //     for (let element of data) {
+  //       let row = table.insertRow();
+  //       for (key in element) {
+  //         let cell = row.insertCell();
+  //         let text = document.createTextNode(element[key]);
+  //         cell.appendChild(text);
+  //       }
+  //     }
+  //   }
+  //   mountains = [];
+  //}
 
-  schoolName = user.displayName;
-  var ctext = document.getElementById('classNameLi').nodevalue;
-  var tasksRef = firebase.database().ref("Schools/liberty/classes/" + ctext + "/Tasks/");
-  tasksRef.on('value', function (snapshot) {
-    console.log(snapshot);
+  var classesRef = firebase.database().ref('classes');
+  var classesList;
+  classesRef.on('value', function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
-      console.log(childSnapshot);
       var childData = childSnapshot.val();
-      tasksRef.on('child_added', function (snapshot) {
+      classesRef.on('child_added', function (snapshot) {
         //Do something with the data
         //document.getElementById("classNameLi").innerHTML = childData.className;
-      });
-      tasks.push(childData);
-      taskList = tasks.toString();
 
+      });
+      classes.push(childData.className);
+      classes.push(childData.Tasks);
     });
-    document.getElementById("taskNameLi").innerHTML = taskList;
-  });//}
- // else {
-    // No user is signed in.
-    //window.location = "http://behavv.com/index.html";
- // }
+    for (i = 0; i < (classes.length / 2); i++) {
+      var td = document.createElement('TABLE');
+      document.getElementById("classNameLi").appendChild(td);
+      mountains = [
+        { StudentName: "Bryce", Bring_Pencils: 1, Bring_Homework: 1, Task4: 3, task3: 9 },
+        { name: "Gage", task1: 4, task2: 5, task3: 3, task4: 6 },
+        { name: "Matt", task1: 2, task2: 2, task3: 3, task4: 6 },
+        { name: "Eddie", task1: 5, task2: 3, task3: 3, task4: 6 },
+        { name: "Pat", task1: 3, task2: 4, task3: 3, task4: 6 }
+      ];
+
+    }
+
+    generateClassesTable(mountains);
+  });
+
+  //log out functionality on top right
+  function signout() {
+    firebase.auth().signOut();
+    alert("signed out");
+  };
+  const sout = document.getElementById("lout");
+  sout.addEventListener('click', signout);
+
+  //gets signed in user
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user != null) {
+      document.getElementById("linu").innerHTML = user.email;
+    } else {
+      document.getElementById("linu");
+    }
+  })
 });
+
+//Show tasks as table
+// firebase.auth().onAuthStateChanged(function (user) {
+//   if (user) {
+//     uid = user.uid;
+//     email = user.email;
+
+//     schoolName = user.displayName;
+//     var ctext = classes[1];//document.getElementById('classNameLi').nodevalue;
+//     //console.log(classes);
+//     var tasksRef = firebase.database().ref("Schools/liberty/classes/" + ctext + "/Tasks/");
+//     tasksRef.on('value', function (snapshot) {
+//       //console.log(snapshot);
+//       snapshot.forEach(function (childSnapshot) {
+//         console.log(childSnapshot);
+//         var childData = childSnapshot.val();
+//         tasksRef.on('child_added', function (snapshot) {
+//           //Do something with the data
+//           //document.getElementById("classNameLi").innerHTML = childData.className;
+//         });
+//         tasks.push(childData);
+//         taskList = tasks.toString();
+
+//       });
+//       document.getElementById("taskNameLi").innerHTML = taskList;
+//     });
+//   }
+//   else {
+//     // No user is signed in.
+//     //window.location = "http://behavv.com/index.html";
+//   }
+// });
 
 // var classRef = firebase.database().ref('Schools/' + schoolName + "/classes");
 //     classRef.on('value', function (snapshot) {
@@ -224,7 +242,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 //         classesList = classes.toString();
 
 //       });
-      
+
 //       for (i = 0; i < classes.length; i++) {
 //         var node = document.createElement('ul');
 //         node.classList.add("classList"); /*adds classList as class on ul*/
@@ -232,7 +250,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 //         node.appendChild(textNode);
 //         document.getElementById("classNameLi").appendChild(node);
 
-       
+
 //       }
 //       //document.getElementById("classNameLi").innerHTML = classesList;
 //     });
@@ -240,4 +258,4 @@ firebase.auth().onAuthStateChanged(function (user) {
 //     // No user is signed in.
 //     // window.location = "http://behavv.com/index.html";
 //   }
-  
+
