dac470e047ea72cc796b9136eef407756b89f8f7
ml1402 <60682008+ml1402@users.noreply.github.com>
2020-11-22 16:30:23 -0500

    changed modal and removed old feature from dashboard

--- public/js/student.js
+++ public/js/student.js
@@ -16,25 +16,25 @@ firebase.initializeApp(firebaseConfig);
 
 // Get the modal
 var studentModal = document.getElementById("studentModal");
-var delStudentModel = document.getElementById("delStudentModel");
+//var delStudentModel = document.getElementById("delStudentModel");
 
 // Get the button that opens the modal
 var addStudentbtn = document.getElementById("addStudentModal");
-var delStudentbtn = document.getElementById("delStuBtn");
+//var delStudentbtn = document.getElementById("delStuBtn");
 
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close2")[0];
-var span3 = document.getElementsByClassName("close3")[0];
+//var span3 = document.getElementsByClassName("close3")[0];
 
 // When the user clicks on the button, open the modal 
 addStudentbtn.onclick = function () {
   studentModal.style.display = "block";
 }
-delStudentbtn.onclick = function () {
-  delStudentModal.style.display = "block";
-}
+// delStudentbtn.onclick = function () {
+//   delStudentModal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
@@ -43,9 +43,9 @@ span.onclick = function () {
 span2.onclick = function () {
   addStuClass.style.display = "none";
 }
-span3.onclick = function () {
-  delStudentModel.style.display = "none";
-}
+// span3.onclick = function () {
+//   delStudentModel.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -117,7 +117,7 @@ function newStudent() {
 function delStudent(){
   var IDList = [];
   var studentID = document.getElementById("studentID").value;
-  delStudentModel.style.display = "none";
+  studentModal.style.display = "none";
   firebase.database().ref('Schools/' + userSchool + "/students/")
 
   for (i = 0; i <= IDList.length; i++) {
